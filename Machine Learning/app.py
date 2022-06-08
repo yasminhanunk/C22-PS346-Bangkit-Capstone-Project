ca0a575dcf5c4492d5c43cf750401ab171f0bc51
cerdasjanastu <cerdasjanastu@gmail.com>
2022-06-08 14:57:37 +0700

    add input for predict

--- Machine Learning/app.py
+++ Machine Learning/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 
@@ -7,6 +8,17 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {"WELCOME": "GO TO /docs route, or /post or send post request to /predict "}
+
+
+@app.get('/predict', response_class=HTMLResponse)
+def take_input():
+    return '''<form method="post">
+    <input type="text" name="doctor" value=""/>
+    <input type="text" name="gender" value=""/>
+    <input type="number" name="age" value=""/>
+    <input type="submit"/>
+    </form>'''
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
